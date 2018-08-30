c30ac35d54bb6c4a42c8a3e4666f4ad77f7d4fa6
Adam Copp <adam.copp@gmail.com>
2018-08-31 00:52:00 +0100

    Refinements to ride screen and theme

--- app/src/main/java/com/esteth/lynes/RideListFragment.kt
+++ app/src/main/java/com/esteth/lynes/RideListFragment.kt
@@ -2,34 +2,39 @@ package com.esteth.lynes
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import kotlinx.android.synthetic.main.fragment_ride_list.*
 
 
 private const val ARG_PARK_ID = "parkId"
 
 /**
- * A simple [Fragment] subclass.
- * Use the [RideListFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
+ * Fragment to display a list of rides within a single park.
  */
 class RideListFragment : Fragment() {
-  private var parkId: String? = null
+    private var parkId: String? = null
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    arguments?.let {
-      parkId = it.getString(ARG_PARK_ID)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            parkId = it.getString(ARG_PARK_ID)
+        }
     }
-  }
 
-  override fun onCreateView(
-      inflater: LayoutInflater,
-      container: ViewGroup?,
-      savedInstanceState: Bundle?): View? {
-    return inflater.inflate(R.layout.fragment_ride_list, container, false)
-  }
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_ride_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        ridesRecycler
+                .addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL))
+    }
 }
